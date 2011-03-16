99742569d9388739e74ba163b8afef3b6b2e6628
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-03-17 04:34:39 +0800

    disable authenticity token again

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -1,6 +1,6 @@
 class PaypalController < ApplicationController
   
-  # skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
   include ActiveMerchant::Billing::Integrations
   
   def ipn
