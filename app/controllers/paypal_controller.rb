d9cc41246e90755d27e0d82f748afb72a3e51ee9
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-06-08 20:16:57 +0800

    - initial transaction mapping - fixed sidebar partial reference and added the app_root for consistency and fix a bug where some pages do not display the appropriate sidebar

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -8,12 +8,18 @@ class PaypalController < ApplicationController
     # require 'ruby-debug'
     # debugger
     notify = Paypal::Notification.new(request.raw_post)
-
+    @transaction = Transaction.new
+    @transaction.status = "NEED_TO_MAP"
+    @transaction.transaction_reference = params[:txn_id]
+    @transaction.transaction_date = Time.now 
+    @transaction.ipn_data = params
+    @transaction.paypal_account_id = params[:payer_email]
+    @transaction.transaction_type = params[:txn_type]
+    @transaction.amount = params[:mc_gross]
     if notify.acknowledge
-      # transaction = Transaction.first
-      # transaction.update_attributes(:status => "Paid") if transaction
+      @transaction.save
     else
-      #DO SOMETHING
+      @transaction.save(false)
     end
     
     render :nothing => true
