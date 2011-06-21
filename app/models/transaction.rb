11826a287eefbf04954d59b30103a21c66f849b6
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-06-21 14:00:43 +0800

    fix that retarded error

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -12,11 +12,11 @@ class Transaction < ActiveRecord::Base
   
   # for non-payment transactions, the ipn_account_id is expected to be nil
   def find_or_create_paypal_account
-    unless ipn_data.nil? or !paypal_account.nil? or ipn_account_email.blank?
-      self.build_paypal_account(:email => self.payer_email,
-        :payer_id => self.payer_id,
-        :first_name => self.ipn_data["first_name"],
-        :last_name => self.ipn_data["last_name"])
+    unless a.ipn_data.nil? or a.paypal_account.nil? or a.payer_email.blank?
+      a.build_paypal_account(:email => a.payer_email,
+        :payer_id => a.payer_id,
+        :first_name => a.ipn_data["first_name"],
+        :last_name => a.ipn_data["last_name"])
     end 
   end
 end
