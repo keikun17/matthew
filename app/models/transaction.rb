882ed2887fcb65367e1b6a144f9b2dc2edd5f25e
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-06-21 14:02:48 +0800

    mou ichido

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -12,8 +12,8 @@ class Transaction < ActiveRecord::Base
   
   # for non-payment transactions, the ipn_account_id is expected to be nil
   def find_or_create_paypal_account
-    unless a.ipn_data.nil? or a.paypal_account.nil? or a.payer_email.blank?
-      a.build_paypal_account(:email => a.payer_email,
+    unless ipn_data.nil? or paypal_account.nil? or payer_email.blank?
+      self.build_paypal_account(:email => payer_email,
         :payer_id => a.payer_id,
         :first_name => a.ipn_data["first_name"],
         :last_name => a.ipn_data["last_name"])
