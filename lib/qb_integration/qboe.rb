01d029b047d70c7909517b841a46e0dc82940762
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-06-22 15:22:52 +0800

    refactor create_paymeny method to allow future modifications [use listid, etc]

--- lib/qb_integration/qboe.rb
+++ lib/qb_integration/qboe.rb
@@ -38,9 +38,9 @@ class Qboe
       create_payment(full_name, txn_id, amount)
   end
   
-  def self.create_payment(full_name, txn_id, amount)
+  def self.create_payment(customer_name, txn_id, amount)
     today = Time.now.strftime("%Y-%m-%d")
-    customer_id = find_customer_id(full_name)
+    customer_id = find_customer_id(customer_name)
     full_name = customer_name
     session = self.getSession
     xml_to_send = ERB.new(get_file_as_string("lib/qb_integration/payment.erb")).result(binding) 
