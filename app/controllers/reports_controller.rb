5d5173a44a7b1711343a5d4c81c014a6403a2b6e
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-06-30 17:12:58 +0800

    reports fixes

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,15 +5,22 @@ class ReportsController < ApplicationController
       @product = Product.find params[:product]
       date_from = Date.parse(params[:date_from].to_a.sort.collect{|c| c[1]}.join("-") )
       date_to = Date.parse(params[:date_to].to_a.sort.collect{|c| c[1]}.join("-") )
-
-      @transactions = Transaction.find(:all,
+      case params[:list_of]
+      when 'invoice'
+      @transactions = Transaction.invoices.find(:all,
         :conditions => ["product = ? and created_at between ? and ? and classification = ?", 
           @product.paypal_product_code,
           date_from.to_s(:db),
-          date_to.to_s(:fb),
-          params[:list_of]])
+          date_to.to_s(:fb)])
+      when 'credit'
+        @transactions.credits.find(:all,
+          :conditions => ["product = ? and created_at between ? and ? and classification = ?", 
+            @product.paypal_product_code,
+            date_from.to_s(:db),
+            date_to.to_s(:fb)])
+      end
     else        
-      @transactions = Transaction.invoices.all
+      @transactions = Transaction.all
     end
   end
 end
