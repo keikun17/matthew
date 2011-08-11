c04373fd414f2803138396dd6039df68ee0c58dc
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-08-11 16:56:31 +0800

    cleanup arels

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,14 +5,16 @@ class ReportsController < ApplicationController
     if params[:product] and params[:date_from] and params[:date_to]
       @product = Product.find params[:product]
       @date_from = params[:date_from] #Date.parse(params[:date_from].to_a.sort.collect{|c| c[1]}.join("-") )
-      @date_to = params[:date_to] #Date.parse(params[:date_to].to_a.sort.collect{|c| c[1]}.join("-") )
+      @date_to = params[:date_to] #Date.parse(params[:date_to].to_a.sort.collect{|c| c[1]}.join("-") ) 
       case params[:list_of]
       when 'invoice'
-        @transactions = Transaction.invoices.page(params[:page]).where(:product => @product.paypal_product_code, :created_at => db_date(@date_from)..db_date(@date_to))
-        @total = Transaction.invoices.where(:product => @product.paypal_product_code, :created_at => db_date(@date_from)..db_date(@date_to)).sum(:amount)
+        @transactions_arel = Transaction.invoices.where(:product => @product.paypal_product_code, :created_at => db_date(@date_from)..db_date(@date_to))
+        @transactions = @transactions_arel.page(params[:page])
+        @total = @transactions_arel.sum(:amount)
       when 'credit'
-        @transactions = Transaction.credits.page(params[:page]).where(:product => @product.paypal_product_code, :created_at => db_date(@date_from)..db_date(@date_to))
-        @total = Transaction.credits.where(:product => @product.paypal_product_code, :created_at => db_date(@date_from)..db_date(@date_to)).sum(:amount)
+        @transactions_arel = Transaction.credits.where(:product => @product.paypal_product_code, :created_at => db_date(@date_from)..db_date(@date_to))
+        @transactions = @transactions_arel.page(params[:page])
+        @total = @transactions_arel.sum(:amount)
       end
     else        
       @transactions = Transaction.page(params[:page])
@@ -23,17 +25,26 @@ class ReportsController < ApplicationController
   def export
     @date_from = db_date(params[:date_from])
     @date_to = db_date(params[:date_to])
+    
+    if params[:product]
+      @product = Product.find params[:product]
+    end
+    
+    @transactions_arel = Transaction.where(:created_at => @date_from..@date_to)
+
     case params[:list_of]
     when 'invoice'
-      @transactions = Transaction.invoices.where(:created_at => @date_from..@date_to).includes([{:paypal_account => :devex_user}])
-      @total = Transaction.invoices.where(:created_at => @date_from..@date_to).sum(:amount)
+      @transactions_arel = @transactions_arel.invoices
     when 'credit'
-      @transactions = Transaction.credits.where(:created_at => @date_from..@date_to).includes([{:paypal_account => :devex_user}])
-      @total = Transaction.credits.where(:created_at => @date_from..@date_to).sum(:amount)
-    else        
-      @transactions = Transaction.where(:created_at => @date_from..@date_to).includes([{:paypal_account => :devex_user}])
-      @total = Transaction.sum(:amount)
+      @transactions_arel = @transactions_arel.credits
     end
+
+    unless @product.nil?
+      @transactions_arel = @transactions_arel.where(:product => @product.paypal_product_code)
+    end
+
+    @transactions = @transactions_arel.includes([{:paypal_account => :devex_user}])
+    @total = @transactions_arel.sum(:amount)
     
     book = Spreadsheet::Workbook.new
     sheet1 = book.create_worksheet
