99d90a40d6612af20bf10a9ff0a5b809f9197489
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-08-11 17:03:17 +0800

    cleanup reports arel

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,20 +2,26 @@ class ReportsController < ApplicationController
   def index
     @date_from = params[:date_from] || 1.month.ago
     @date_to = params[:date_to] || 1.days.from_now
-    if params[:product] and params[:date_from] and params[:date_to]
-      @product = Product.find params[:product]
-      @date_from = params[:date_from] #Date.parse(params[:date_from].to_a.sort.collect{|c| c[1]}.join("-") )
-      @date_to = params[:date_to] #Date.parse(params[:date_to].to_a.sort.collect{|c| c[1]}.join("-") ) 
-      case params[:list_of]
-      when 'invoice'
-        @transactions_arel = Transaction.invoices.where(:product => @product.paypal_product_code, :created_at => db_date(@date_from)..db_date(@date_to))
-        @transactions = @transactions_arel.page(params[:page])
-        @total = @transactions_arel.sum(:amount)
-      when 'credit'
-        @transactions_arel = Transaction.credits.where(:product => @product.paypal_product_code, :created_at => db_date(@date_from)..db_date(@date_to))
-        @transactions = @transactions_arel.page(params[:page])
-        @total = @transactions_arel.sum(:amount)
-      end
+    if params[:date_from] and params[:date_to] 
+      if params[:product] and params[:product] != "ALL"
+         @product = Product.find params[:product]
+       end
+
+       @transactions_arel = Transaction.where(:created_at => @date_from..@date_to)
+
+       case params[:list_of]
+       when 'invoice'
+         @transactions_arel = @transactions_arel.invoices
+       when 'credit'
+         @transactions_arel = @transactions_arel.credits
+       end
+
+       unless @product.nil?
+         @transactions_arel = @transactions_arel.where(:product => @product.paypal_product_code)
+       end
+
+       @transactions = @transactions_arel.page(params[:page]).includes([{:paypal_account => :devex_user}])
+       @total = @transactions_arel.sum(:amount)
     else        
       @transactions = Transaction.page(params[:page])
       @total = Transaction.sum(:amount)
@@ -26,7 +32,7 @@ class ReportsController < ApplicationController
     @date_from = db_date(params[:date_from])
     @date_to = db_date(params[:date_to])
     
-    if params[:product]
+    if params[:product] and params[:product] != "ALL"
       @product = Product.find params[:product]
     end
     
