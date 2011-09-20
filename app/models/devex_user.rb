73f609b720b782c5504ec6627263974bba199fa7
Buddy Magsipoc <Mengsk@Buddy1a2a3a.local>
2011-09-20 16:23:11 +0800

    automap feature autoupload mapped transactions to qb

--- app/models/devex_user.rb
+++ app/models/devex_user.rb
@@ -2,12 +2,20 @@
 class DevexUser < ActiveRecord::Base
   has_many :paypal_accounts
   has_many :transactions, :through => :paypal_accounts
-  
+
+  after_save :upload_transactions_to_quickbooks
+
   cattr_reader :per_page
   @@per_page = 20
   
   def full_name
     [first_name, last_name].join(" ")
+  end
+  
+  def upload_transactions_to_quickbooks
+    self.transactions.each do |transaction|
+      transaction.upload_to_quickbooks
+    end
   end
   
 end
--- app/models/paypal_account.rb
+++ app/models/paypal_account.rb
@@ -2,11 +2,24 @@ class PaypalAccount < ActiveRecord::Base
   has_many   :transactions
   belongs_to :devex_user
   validates :first_name, :last_name,:email, :presence => true
-    
+
   scope :orphans, :conditions => "devex_user_id is null"
+
+  after_create :automap_devex_account, :if => Proc.new { |paypal_account| paypal_account.devex_user.nil? and AUTO_MAP_PAYPAL_ACCOUNT_TO_QUICKBOOKS }
   
   def full_name
     [first_name, last_name].join(" ")
+  end
+  
+  def automap_devex_account
+    user = User.find_by_username(self.email)
+    if !user.nil?
+      devex_user.create(:account_id => user.consultant.id,
+        :username => user.username,
+        :account_type => "Consultant",
+        :first_name => user.consultant.first_name,
+        :last_name => user.consultant.last_name)
+    end
   end
   
 end
